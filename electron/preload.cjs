a0be0602b08dbc0e071027b078c81cb652536394
mhuzaifaqpk-debug <m.huzaifa.q.pk@gmail.com>
2026-08-26 12:46:13 +0500

    Add Electron export bridge

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -1,5 +1,7 @@
-const { contextBridge } = require('electron');
+const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
   isElectron: true,
+  saveExport: (buffer, format, suggestedName) =>
+    ipcRenderer.invoke('save-export', { buffer, format, suggestedName }),
 });
